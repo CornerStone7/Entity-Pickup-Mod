45135a0065feaf049420fb3b451e5594d8590261
CornerStone07 <kiehung7@gmail.com>
2024-12-11 14:47:57 +0800

    1.21

--- src/main/java/com/livinglemming/Events/RightClickEventListener.java
+++ src/main/java/com/livinglemming/Events/RightClickEventListener.java
@@ -1,21 +1,22 @@
 package com.livinglemming.Events;
 
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.minecraft.block.Blocks;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.LoreComponent;
+import net.minecraft.component.type.NbtComponent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.nbt.NbtString;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+
+import java.util.List;
 
 public class RightClickEventListener
 {
@@ -29,35 +30,43 @@ public class RightClickEventListener
                 {
                     EntityType<?> entityType = entity.getType();
                     Item spawnEgg = SpawnEggItem.forEntity(entityType);
-                    if (spawnEgg != null)
-                    {
+                    if (spawnEgg != null) {
                         ItemStack spawnEggStack = new ItemStack(spawnEgg);
 
                         // Get Entity NBT.
                         NbtCompound nbt = new NbtCompound();
                         entity.writeNbt(nbt);
 
+                        // Add "id" key to identify the entity type.
+                        String entityId = EntityType.getId(entity.getType()).toString(); // Get entity's ID (e.g., "minecraft:villager").
+                        nbt.putString("id", entityId);
+
                         // Remove attributes to enable cloning of same NBT entity.
                         nbt.remove("UUID");
                         nbt.remove("Pos");
                         nbt.remove("Motion");
                         nbt.remove("Rotation");
+                        nbt.remove("onGround");
 
-                        NbtCompound nbtCompound = new NbtCompound();
-                        nbtCompound.put("EntityTag", nbt);
+                        NbtComponent entityComponent = NbtComponent.of(nbt);
 
                         // Lore Attribute.
+                        List<Text> loreLines = null;
                         if (entity instanceof VillagerEntity villager)
                         {
-                            NbtCompound textCompound = new NbtCompound();
-                            NbtList tooltipList = new NbtList();
-
-                            tooltipList.add(NbtString.of("{\"text\":\"Profession: [" + villager.getVillagerData().getProfession().toString() + "]\",\"color\":\"gray\",\"italic\":false}"));
-                            textCompound.put("Lore", tooltipList);
-                            nbtCompound.put("display", textCompound);
+                            loreLines = List.of(
+                                    Text.literal("Profession: [" + villager.getVillagerData().getProfession().toString() + "]")
+                                    );
                         }
 
-                        spawnEggStack.setNbt(nbtCompound);
+                        // Create LoreComponent.
+                        if (loreLines != null)
+                        {
+                            LoreComponent loreComponent = new LoreComponent(loreLines);
+                            spawnEggStack.set(DataComponentTypes.LORE, loreComponent);
+                        }
+
+                        spawnEggStack.set(DataComponentTypes.ENTITY_DATA, entityComponent);
 
                         // Drop item if inventory is full.
                         if (playerEntity.getInventory().getEmptySlot() != -1)
